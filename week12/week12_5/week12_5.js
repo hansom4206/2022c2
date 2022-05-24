4a3ee98db4a7eabbdbac3fcf9317d76cea3f28a5
eason <95411439+hansom4206@users.noreply.github.com>
2022-05-24 17:01:08 +0800

    Update week12_5.js

--- week12/week12_5/week12_5.js
+++ week12/week12_5/week12_5.js
@@ -15,14 +15,20 @@ function draw() {
     if (choice == 1)
         rect(50, 100, 150, 2);
     if (choice == 2)
-        rect(50, 100, 150, 2);
+        rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("中餐吃泡麵", 50, 50);
     text("中餐Uber Eat", 50, 100);
     text("中餐Food Panda", 50, 150);
+    text("中餐吃牛排", 50, 200);
+    text("中餐吃大餐", 50, 250);
 }
 
 function mousePressed() {
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
